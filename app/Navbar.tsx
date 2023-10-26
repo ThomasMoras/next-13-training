a9f7eb15a14184d56039580c66fa10494a0805fc
Thomas <moras.thomas@gmail.com>
2023-10-27 00:24:29 +0200

    [YT_2] Styling navbar link

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,8 +1,13 @@
+"use client";
+
 import Link from "next/link";
 import React from "react";
 import { FaBtc } from "react-icons/fa";
+import { usePathname } from "next/navigation";
+import classnames from "classnames";
 
 const Navbar = () => {
+  const currentPath = usePathname();
   const linkArray = [
     { href: "/", label: "Dashboard" },
     { href: "/issues", label: "Issues" },
@@ -15,7 +20,11 @@ const Navbar = () => {
       <ul className="flex space-x-6 ">
         {linkArray.map((link) => (
           <li
-            className="text-blue-400 hover:text-blue-800 transition-colors"
+            className={classnames({
+              "text-blue-900 underline": link.href === currentPath,
+              "text-blue-500": link.href != currentPath,
+              "hover:text-blue-800 transition-colors": true,
+            })}
             key={link.href}
           >
             <Link href={link.href}>{link.label}</Link>
