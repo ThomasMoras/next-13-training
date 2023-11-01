bf792b3de7687b006ff619d851a7ce801f5d55f4
Thomas <moras.thomas@gmail.com>
2023-11-01 23:48:34 +0100

    Form submition

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -2,16 +2,43 @@
 
 import { Button, TextArea, TextField } from "@radix-ui/themes";
 import React from "react";
+import { Controller, useForm } from "react-hook-form";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+interface IssueForm {
+  title: string;
+  description: string;
+}
 
 const NewIssue = () => {
+  const router = useRouter();
+  const { register, control, handleSubmit } = useForm<IssueForm>();
+  console.log(register);
+
   return (
-    <div className="max-w-lg space-y-3">
+    <form
+      className="max-w-lg space-y-3"
+      onSubmit={handleSubmit(async (data) => {
+        console.log(data);
+        await axios.post("/api/issues", data);
+        router.push("/issues");
+      })}
+    >
       <TextField.Root>
-        <TextField.Input placeholder="Title"></TextField.Input>
+        <TextField.Input
+          placeholder="Title"
+          {...register("title")}
+        ></TextField.Input>
       </TextField.Root>
-      <TextArea placeholder="Description"></TextArea>
+      <Controller
+        name="description"
+        control={control}
+        render={({ field }) => (
+          <TextArea placeholder="Description" {...field}></TextArea>
+        )}
+      ></Controller>
       <Button>Submit</Button>
-    </div>
+    </form>
   );
 };
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
 import "@radix-ui/themes/styles.css";
+import "./theme-config.css";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Navbar from "./Navbar";
 import { Theme } from "@radix-ui/themes";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -18,8 +19,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html data-theme="winter" lang="en">
-      <body className={inter.className}>
+    <html data-theme="winter" lang="en" className={inter.variable}>
+      <body>
         <Theme>
           <Navbar></Navbar>
           <main className="p-5"> {children}</main>
