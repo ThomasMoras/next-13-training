053ea450d8578841ffbe57807948ee5501b88809
Thomas <moras.thomas@gmail.com>
2023-11-02 16:23:56 +0100

    Schema & Handle errors

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,44 +1,72 @@
 "use client";
 
-import { Button, TextArea, TextField } from "@radix-ui/themes";
-import React from "react";
+import { Button, Callout, Text, TextArea, TextField } from "@radix-ui/themes";
+import React, { useState } from "react";
 import { Controller, useForm } from "react-hook-form";
 import axios from "axios";
 import { useRouter } from "next/navigation";
-interface IssueForm {
-  title: string;
-  description: string;
-}
+import { zodResolver } from "@hookform/resolvers/zod";
+import { createIssueSchema } from "@/app/createIssueSchema";
+import z from "zod";
+
+type IssueForm = z.infer<typeof createIssueSchema>;
 
 const NewIssue = () => {
   const router = useRouter();
-  const { register, control, handleSubmit } = useForm<IssueForm>();
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema),
+  });
+  const [error, setError] = useState("");
   console.log(register);
 
   return (
-    <form
-      className="max-w-lg space-y-3"
-      onSubmit={handleSubmit(async (data) => {
-        console.log(data);
-        await axios.post("/api/issues", data);
-        router.push("/issues");
-      })}
-    >
-      <TextField.Root>
-        <TextField.Input
-          placeholder="Title"
-          {...register("title")}
-        ></TextField.Input>
-      </TextField.Root>
-      <Controller
-        name="description"
-        control={control}
-        render={({ field }) => (
-          <TextArea placeholder="Description" {...field}></TextArea>
+    <div className="max-w-lg">
+      {error && (
+        <Callout.Root color="red" className="mb-5">
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form
+        className="space-y-3"
+        onSubmit={handleSubmit(async (data) => {
+          console.log(data);
+          try {
+            await axios.post("/api/issues", data);
+            router.push("/issues");
+          } catch (error) {
+            console.log(error);
+            setError("An unexpected error occurred.");
+          }
+        })}
+      >
+        <TextField.Root>
+          <TextField.Input
+            placeholder="Title"
+            {...register("title")}
+          ></TextField.Input>
+        </TextField.Root>
+        {errors.title && <Text color="red">{errors.title.message}</Text>}
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <TextArea placeholder="Description" {...field}></TextArea>
+          )}
+        ></Controller>
+        {errors.description && (
+          <Text color="red" as="p">
+            {errors.description.message}
+          </Text>
         )}
-      ></Controller>
-      <Button>Submit</Button>
-    </form>
+
+        <Button>Submit</Button>
+      </form>
+    </div>
   );
 };
 
